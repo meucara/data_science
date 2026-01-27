1e9102bfb2bcad755406c354e75aa5383d1ca10f
meucara <linusaxelsson.0501@gmail.com>
2026-01-27 23:09:46 +0100

    Update data_cleaning.py
    Comments update

--- pages/data_cleaning.py
+++ pages/data_cleaning.py
@@ -8,30 +8,34 @@ from pipeline import graphs, get_cleaned_data
 
 df, df_filtered, mode_counts, stats, df_classic, top_10_wins, bottom_10_wins, df_champions, df_duration, df_match_agg = get_cleaned_data()
 
-if not stats.empty:
-    st.write("### DATA CLEANING")
-    st.info("""
-    **Definitioner för matchlängd:**
-    * **Full Game:** Över 15 minuter (Matchen spelades troligtvis klart).
-    * **Early Surrender:** Mellan 3,5 och 15 minuter (Högst troligt att någon lämnat).
-    * **Remake:** Under 3,5 minuter (Garanterat att någon lämnat).
-    """)
-    st.write("""För att få en tydligare bild av matcherna och få mer intressant data att titta på,
-     kommer fokus enbart att ligga på "Full Game".""")
+fig1, fig2, fig3, fig4, fig5, fig6, fig_agg = graphs(stats, mode_counts, top_10_wins, bottom_10_wins, df_champions,
+                                                     df_duration, df_match_agg, df_classic)
 
-    st.write("""Under följer data för att visa på hur många matcher datan innehåller och fördelningen av de olika "matchtyperna" 
-     """)
+st.header("DATA CLEANING")
 
-    st.dataframe(stats, hide_index=True, use_container_width=True)
-
-    # Anropa funktionen för att hämta figuren
-    fig1, fig2, fig3, fig4, fig5, fig6, fig_agg = graphs(stats, mode_counts, top_10_wins, bottom_10_wins, df_champions,
-                                                         df_duration, df_match_agg, df_classic)
-
-    # Visa figuren i Streamlit
-    st.pyplot(fig1)
+st.write("""I denna del så analyseras datan för att städa bort det som inte är relevant för kommande analyser.
+Fokus här ligger på att hitta matcher som inte är fullständiga och matcher som inte är av samma typ.""")
 
 
+st.info("""
+**Definitioner för matchlängd:**
+* **Full Game:** Över 15 minuter (Matchen spelades troligtvis klart).
+* **Early Surrender:** Mellan 3,5 och 15 minuter (Högst troligt att någon lämnat).
+* **Remake:** Under 3,5 minuter (Garanterat att någon lämnat).
+""")
+st.write("""För att få en tydligare bild av matcherna och få mer intressant data att titta på,
+ kommer fokus enbart att ligga på "Full Game".""")
+
+st.write("""Under följer data för att visa på hur många matcher datan innehåller och fördelningen av de olika "matchtyperna" 
+ """)
+
+st.dataframe(stats, hide_index=True, use_container_width=True)
+
+st.pyplot(fig1)
+
+
+st.write("I datan finns det även olika typer av modes (typer av spel inom League Of Legends)."
+         "Ut av de 3828 games som är kvar efter första städningen med matchens längd görs ännu en städning där analysen sedan fokuserar enbart på Classic mode.")
 
 st.info("""
     **Olika Game Modes:**
@@ -40,7 +44,7 @@ st.info("""
     * **SWIFTPLAY** = Ett mellanting mellan Aram och Classic där det spelas på CLASSIC bana men allt går mycket fortare.
     """)
 
-if not mode_counts.empty:
-    st.dataframe(mode_counts, hide_index=True, use_container_width=True)
 
-    st.pyplot(fig2)
+st.dataframe(mode_counts, hide_index=True, use_container_width=True)
+
+st.pyplot(fig2)
